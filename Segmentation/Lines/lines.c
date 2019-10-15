ac00039ca264c3f876944b49b1eba9306b2eba63
root <root@ASUS_ZenBook.localdomain>
2019-10-15 23:35:18 +0200

    update

---
+++ Segmentation/Lines/lines.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <err.h>
+
+int is_blank_line(SDL_Surface *image, int index) 
+{
+	int height = image -> height;
+	int width = image -> width;
+	int x = 0;
+	int y = 0;
+	Uint32 pixel = get_pixel
+	while (
+}
+
+// Rajoute les lignes rouges
+void split_lines(SDL_Surface *image)
+{
+	int height = image -> height;
+	int width = image -> width;
+}
+
---
+++ Segmentation/Lines/lines.h
@@ -0,0 +1,8 @@
+#ifndef LINES_H
+#define LINES_H
+
+
+int is_blank_line(SDL_Surface *suface,int index);
+void split_lines(SDL_Surface *surface);
+
+#endif
