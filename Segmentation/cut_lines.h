2b772e9a4d67be46d93636f9785c35030e7735ae
root <root@ASUS_ZenBook.localdomain>
2019-10-15 19:22:54 +0200

    add some directory

--- Segmentation/cut_lines.h
+++
@@ -1,7 +0,0 @@
-#ifndef CUT_LINES_H
-#define CUT_LINES_H
-
-int cut_lines(array[][]);
-
-
-#endif
--- Segmentation/cut_paraph.h
+++
@@ -1,9 +0,0 @@
-#ifndef CUT_PARAPH_H
-#define CUT_PARAPH_H
-
-void detect_paraph(int image[][], size_t height, size_t width, File f);
-
-void cut_paraph(int image[][], size_t height, size_t width, indexs[]);
-
-
-#endif
