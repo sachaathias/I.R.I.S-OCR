efdf97ca93918e61d693f562118a3d35d3a29eb8
Marc Lagoin <marc.lagoin@epita.fr>
2019-10-18 18:14:42 +0200

    20%

--- neural_networks/xor_neural_networks.c
+++ neural_networks/xor_neural_networks.c
@@ -3,17 +3,34 @@
 #include <err.h>
 #include <math.h>
 #include <time.h>
+#include "xor_neural_networks.h"
 
 
 
 int training[4][2] = {{0, 0}, {0, 1}, {1, 0}, {1, 1}};
 int anwser[4] = {0, 1, 1, 0};
 
-double weight_hidden[4];
-double weight_output[1];
-float biases[2];
-float error[3];
-float output;
+struct Network
+{
+//number of inputs and number of neurons in hidden layer
+	int inputs;
+	int hidden;
+//arrays of weights and biases
+	double *weight_hidden;
+	double *weight_output;
+	double *biases_hidden;
+	double bias_output;
+//output of hidden layer
+	double *output_hidden;
+//error rate and output of the network
+	float error;
+	float output;
+//training data
+	int *training;
+//network goal anwser
+	int *anwser;
+}
+
 
 //the sigmoid function
 float sigmoid(float x)
@@ -27,37 +44,25 @@ float sigmoid_derivative(float x)
 	return exp(-x)/pow((1 + exp(-x), 2));
 }
 
-//initialize random weights 
-void initialize_weights()
+//initialize the struct and allocate the memory
+struct Initialize_network()
 {
-	srand(time(NULL));
-	for (int i = 0; i < 6; i++)
-	{
-		int randNum = rand() % 2;
-		if (randNum == 1)
-			weights[i] = -1 * (double(rand()) / (double(RAND_MAX) + 1.0)); // generate number between -1.0 and 0.0
-		else
-			weights[i] = double(rand()) / (double(RAND_MAX) + 1.0); // generate number between 1.0 and 0.0
-	}
-	
-}
-
-void initialize_bias()
-{
-	srand(time(NULL));
-	for (int i = 0; i < 2; i++)
-	{
-		biases[i] = double(rand()) / (double(RAND_MAX) + 1.0);
-	}
-}
+	struct Network self;
+	self.inputs = 2;
+	self.hidden = 3;
+	self.weight_hidden = malloc(sizeof(double)*self.inputs*self.hidden);
+	self.weight_output = malloc(sizeof(double)*self.hidden);
+	self.biases_hidden = malloc(sizeof(double)*self.hidden);
+	self.bias_output = (double)rand()/(double)RAND_MAX;
+	self.output_hidden = malloc(sizeof(double)*self.hidden);
+	self.error = 0.0;
+	self.output = 0.0;
+	self.training = malloc(sizeof(int)*4*2);
+	self.anwser = malloc(sizeof(int)*4);
 
 
 
-float cost_function(float weight, float bias)
-{
-	return 0;
+
+
 
 }
-
-
-
