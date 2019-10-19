29897cea698053ee0c29655f163c890ec4eaf0a1
Etienne Chanudet <etienne.chanudet@epita.fr>
2019-10-19 22:09:31 +0200

    envoi

--- XOR/nn_xor.c
+++ XOR/nn_xor.c
@@ -11,6 +11,10 @@ typedef struct
 	double *hidden_value;
 	double output_value;
 
+	//Biais of each layer
+	double *bias_hidden;
+	double bias_output;
+
 	//Use for error_rate and learning
 	double *goal_output;
 
@@ -20,16 +24,7 @@ typedef struct
 
 	double error_rate;
 
-	//Delta of each weight for learning
-	double *delta_weight_input_hidden;
-	double *delta_weight_hidden_output;
-
-	//Delta of each value in each layer
-	double delta_output_value;
-	double *delta_hidden_value;
-
 	double learning_rate;
-	double alpha;
 } neural_net;
 
 //Return a random number
@@ -50,6 +45,9 @@ neural_net init_net()
 	net.hidden_value = malloc(sizeof(double) * net.nb_hidden);
 	net.output_value = 0.0;
 
+	net.bias_hidden = malloc(sizeof(double) * net.nb_hidden);
+	net.bias_output = 0.0;
+
 	net.goal_output = malloc(sizeof(double) * 4);
 
 	net.weight_input_hidden = malloc(sizeof(double) * net.nb_input * net.nb_hidden);
@@ -57,14 +55,7 @@ neural_net init_net()
 
 	net.error_rate = 0.0;
 
-	net.delta_weight_input_hidden = malloc(sizeof(double) * net.nb_input * net.nb_hidden);
-	net.delta_weight_hidden_output = malloc(sizeof(double) * net.nb_hidden);
-
-	net.delta_output_value = 0.0;
-	net.delta_hidden_value = malloc(sizeof(double) * net.nb_hidden);
-
-	net.learning_rate = 0.5;
-	net.alpha = 0.9;
+	net.learning_rate = 0.01;
 
 	return net;
 }
@@ -91,21 +82,34 @@ void init_value(neural_net *net)
 		for(int h = 0; h < net->nb_hidden; h++)
 		{
 			*(net->weight_input_hidden + (h + i * net->nb_hidden)) = random();
-			*(net->delta_weight_input_hidden + (h + i * net->nb_hidden)) = 0.0;
 		}
 	}
 
 	for(int h = 0; h < net->nb_hidden; h++)
 	{
 		*(net->weight_hidden_output + h) = random();
-		*(net->delta_weight_hidden_output + h) = 0.0;
+		*(net->bias_hidden + h) = random();
 	}
+	net->bias_output = random();
 }
 
 //Return the Sigmoid of a number
 double sigmoid(double x)
 {
 	return 1/(1+exp(-x));
+}
+
+double sigmoid_d(double x)
+{
+	//return exp(-x) / ((1 + exp(-x)) * (1 + exp(-x)));
+	return x * (1.0 - x);
+}
+
+//Return the error rate
+double error_rate(double expected_output, double output)
+{
+	//return 0.5 * (expected_output - output) * (expected_output - output);
+	return sigmoid(output) * (expected_output - output);
 }
 
 //Forward propagation for neural net
@@ -119,7 +123,7 @@ void feed_forward(neural_net *net, int p, int print)
 			sum_hidden += *(net->input_value + 2 * p  + i) *
 			*(net->weight_input_hidden + (h + i * net->nb_hidden));
 		}
-		*(net->hidden_value + i) = sigmoid(sum_hidden);
+		*(net->hidden_value + i) = sigmoid(sum_hidden + *(net->bias_hidden + i));
 	}
 
 	double sum_output = 0.0;
@@ -127,11 +131,9 @@ void feed_forward(neural_net *net, int p, int print)
 	{
 		sum_output += *(net->hidden_value + h) * *(net->weight_hidden_output + h);
 	}
-	net->output_value = sigmoid(sum_output);
+	net->output_value = sigmoid(sum_output + net->bias_output);
 
-	net->error_rate = 0.5 * (*(net->goal_output + p)
-		- net->output_value) * (*(net->goal_output + p)
-		- net->output_value);
+	net->error_rate = error_rate(*(net->goal_output + p), net->output_value);
 
 	if(print % 100 == 0)
 	{
@@ -143,9 +145,32 @@ void feed_forward(neural_net *net, int p, int print)
 }
 
 //Backward propagation (learning) for neural net
-void backward_propagation()
+void backward_propagation(neural_net *net, int p)
 {
-	//FIXME
+	//Update weight of hidden to output layer
+	*(net->weight_hidden_output) += net->error_rate
+		* net->learning_rate * *(net->hidden_value);
+	*(net->weight_hidden_output + 1) += net->error_rate
+		* net->learning_rate * *(net->hidden_value + 1);
+	//Update bias of output layer
+	net->bias_output += net->error_rate * net->learning_rate;
+
+	//Calculate error for each hidden neuron
+	double e1 = sigmoid_d(*(net->hidden_value))
+		* net->error_rate * *(net->weight_hidden_output);
+	double e2 = sigmoid_d(*(net->hidden_value + 1))
+		* net->error_rate * *(net->weight_hidden_output + 1);
+	
+
+	//Adjust weight of neurons
+	*(net->weight_input_hidden) += e1 * *(net->input_value + 2 * p) * net->learning_rate;
+	*(net->weight_input_hidden + 1) += e1 * *(net->input_value + 2 * p + 1) * net->learning_rate;
+	*(net->weight_input_hidden + 2) += e2 * *(net->input_value + 2 * p) * net->learning_rate;
+	*(net->weight_input_hidden + 3) += e2 * *(net->input_value + 2 * p + 1) * net->learning_rate;
+
+	//Adjust bias
+	*(net->bias_hidden) += e1 * net->learning_rate;
+	*(net->bias_hidden + 1) += e2 * net->learning_rate;
 }
 
 //Main function of XOR neural net
@@ -153,16 +178,29 @@ void XOR()
 {
 	srand(time(NULL));
 	
-	int trainMax = 10000;
+	int trainMax = 100000000;
 	neural_net n = init_net();
 	neural_net *net = &n;
 	init_value(net);
 
+	*(net->weight_input_hidden) = 20;
+	*(net->weight_input_hidden + 1) = 20;
+	*(net->weight_input_hidden + 2) = -20;
+	*(net->weight_input_hidden + 3) = -20;
+
+	*(net->bias_hidden) = -10;
+	*(net->bias_hidden + 1) = 30;
+
+	*(net->weight_hidden_output) = 20;
+	*(net->weight_hidden_output + 1) = 20;
+
 	for(int print = 0; print < trainMax; print++)
 	{
+	//	net->error_rate = 0.0;
 		for(int p = 0; p < 4; p++)
 		{
 			feed_forward(net, p, print);
+			//backward_propagation(net, p);
 		}
 	}
 }
