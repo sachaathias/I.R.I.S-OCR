45133d87439b5120697da7cc6eb5745c9885f948
Etienne Chanudet <etienne.chanudet@epita.fr>
2019-10-19 11:50:36 +0200

    envoi

--- Segmentation/Crop/crop.c
+++ Segmentation/Crop/crop.c
@@ -10,7 +10,8 @@ void crop_picture(SDL_Surface* image, int x, int y, int width, int height)
 {
 	SDL_Rect b ={x, y, width, height};
 	SDL_UnlockSurface(image);
-	SDL_Surface *bb = SDL_CreateRGBSurface(SDL_HWSURFACE, b.w, b.h, 32, 0, 0, 0, 0);
+	SDL_Surface *bb = NULL;
+	bb = SDL_CreateRGBSurface(0, b.w, b.h, image->format->BitsPerPixel, 0, 0, 0, 0);
 	SDL_BlitSurface(image,&b,bb,NULL);
 	SDL_LockSurface(image);
 	SDL_SaveBMP(bb,"image.bmp");
