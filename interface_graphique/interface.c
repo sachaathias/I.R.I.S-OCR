37c8641d25e75bbd7b5a300ee2fab8b9674a2472
Marc Lagoin <marc.lagoin@epita.fr>
2019-12-04 18:18:28 +0100

    interface quit

--- interface_graphique/interface.c
+++ interface_graphique/interface.c
@@ -1,5 +1,13 @@
 #include <gtk/gtk.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <gtk/gtkx.h>
+#include <math.h>
+#include <string.h>
+#include <stdio.h>
 
+GtkWidget *window_main;
+GtkWidget *btnShowImage;
 GtkWidget *btnSegmentation;
 GtkWidget *btnTrain;
 GtkWidget *btnStart;
@@ -7,51 +15,47 @@ GtkWidget *btnQuit;
 GtkWidget *btnSelectFile;
 GtkWidget *textBox;
 GtkWidget *previewImage;
+GtkBuilder *builder;
 
-int UI(int argc, char *argv [])
+int main(int argc, char *argv [])
 {
-    GtkWidget *main_window = NULL;
-    SGlobalData data;
-    GError *error = NULL;
-    gchar *filename = NULL;
-
     gtk_init(&argc, &argv);
-    data.builder = gtk_builder_new();
+    builder = gtk_builder_new_from_file("interface.glade");
+    window_main = GTK_WIDGET(gtk_builder_get_object(builder, "window_main"));
 
-    filename = g_build_filename("interface.glade", NULL);
-    gtk_builder_add_from_file(data.builder, filename, &error);
-    g_free(filename);
+    gtk_builder_connect_signals(builder, NULL);
 
-    if (error)
-    {
-        gint code = error->code;
-        g_printerr("%s\n", error->message);
-        g_error_free(error);
-        return code;
-    }
+    btnSegmentation = GTK_WIDGET(gtk_builder_get_object(builder, "BT_SEGMENTATION"));
+    btnTrain = GTK_WIDGET(gtk_builder_get_object(builder, "BT_TRAIN"));
+    btnStart = GTK_WIDGET(gtk_builder_get_object(builder, "BT_START"));
+    btnQuit = GTK_WIDGET(gtk_builder_get_object(builder, "BT_QUIT"));
+    textBox = GTK_WIDGET(gtk_builder_get_object(builder, "textview1"));
+    previewImage = GTK_WIDGET(gtk_builder_get_object(builder, "preview"));
+    btnSelectFile = GTK_WIDGET(gtk_builder_get_object(builder, "BT_CHOOSE"));
+    btnShowImage = GTK_WIDGET(gtk_builder_get_object(builder, "BT_SHOW"));
 
-    gtk_builder_connect_signals(data.builder, &data);
-    main_window = GTK_WIDGET(gtk_builder_get_object(data.builder, "window_main"));
-    gtk_widget_show_all(main_window);
-
-    text_view = (GtkTextView*)gtk_builder_get_object(data.builder, "textview1");
-    buffer = gtk_text_view_get_buffer(text_view);
-    gtk_text_buffer_set_text(buffer, "", strlen(""));
-
-    btnSegmentation = GTK_WIDGET(gtk_builder_get_object(data.builder, "BT_SEGMENTATION"));
-    btnTrain = GTK_WIDGET(gtk_builder_get_object(data.builder, "BT_TRAIN"));
-    btnStart = GTK_WIDGET(gtk_builder_get_object(data.builder, "BT_START"));
-    btnQuit = GTK_WIDGET(gtk_builder_get_object(data.builder, "BT_QUIT"));
-    textBox = GTK_WIDGET(gtk_builder_get_object(data.builder, "textview1"));
-    previewImage = GTK_WIDGET(gtk_builder_get_object(data.builder, "preview"));
-    btnSelectFile = GTK_WIDGET(gtk_builder_get_object(data.builder, "BT_CHOOSE"));
+    gtk_widget_show(window_main);
 
     gtk_main();
 
     return EXIT_SUCCESS;
 }
 
-void interface_quit(GtkWidget* w, gpointer p_data)
+void on_btnShowImage_clicked(GtkButton *b)
+{
+}
+void on_start_clicked(GtkButton* b)
+{
+
+}
+
+void on_segmentation_clicked(GtkButton* b)
+{
+
+
+}
+
+void on_quit_clicked(GtkButton* b)
 {
     gtk_main_quit();
 }
