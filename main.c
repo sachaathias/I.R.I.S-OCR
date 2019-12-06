71f882de715659807061711195d3c9f66441a85d
sacha athias <sacha.athias@ epita.fr>
2019-12-06 16:46:33 +0100

    update main.c

--- main.c
+++ main.c
@@ -6,53 +6,67 @@
 #include "XOR/xor.h"
 #include "neural_network/main_nn.h"
 
+
+void help()
+{
+    printf("##################################################################\n");
+    printf("#    Welcome to I.R.I.S's OCR V2                                 #\n\
+#              Here is some commands :                           #\n");
+    printf("#       %s--help%s  To picture commands                              #\n",GREEN,DEFAULT);
+    printf("#       %s--XOR%s  To show the neural network for the XOR function   # \n",GREEN,DEFAULT);
+    printf("#       %s--img_trt <image_path>%s To run                            #\n\
+#              binarisation and segmentation                     #\n",GREEN,DEFAULT);
+    printf("#       %s--start%s  Launch GUI                                      #\n",GREEN,DEFAULT);
+    printf("##################################################################\n");
+}
+
+
 int main(int argc, char** argv)
 {
-/*	if(argc != 2)
-		errx(1, "No file specified");
-	char* file = argv[1];
-	display_image_test(file);
-	XOR();
-	return 0;
-*/
-	char* error = " * Use \'-xor\' to run XOR neural network\n * Use \'-img_trt <image_path>\' to run\n\tbinarisation and segmentation";
-	if(argc < 2)
-	{
-		//printf("Not enough arguments\n%s\n", error);
+    if(argc < 2)
+    {
+        printf("Not enough arguments:\n\
+                \tuse --help to show each commands\n");
+        return 0;
+    }
+     else
+    {
+        for(int i = 1; i < argc; i++)
+        {
+            char* arg = argv[i];
+            if (strcmp(arg,"--help")==0)
+            {
+                help();
+            }
+            else if (strcmp(arg,"--start")==0)
+            {
+                //Call GUI
+            }
+            else if(strcmp(arg, "--XOR") == 0)
+            {
+                xor();
+            }
+            else if(strcmp(arg, "--Train")==0)
+            {
+                train();
+            }
+            else if(strcmp(arg, "--img_trt") == 0)
+            {
+                if(i+1 > argc)
+                    printf("No file specified\n");
+                else
+                {
+                    char* file = argv[i+1];
+                    i++;
+                    display_image_test(file);
+                }
+            }
+            else
+            {
+                printf("Unknown argument \"%s\" use --help to show commands", arg);
+            }
+        }
+        return 0;
+    }
 
-
-
-		return 0;
-	}
-	else
-	{
-		for(int i = 1; i < argc; i++)
-		{
-			char* arg = argv[i];
-			if(strcmp(arg, "-xor") == 0)
-			{
-				xor();
-			}
-			else if(strcmp(arg, "-train") == 0)
-			{
-				train();
-			}
-			else if(strcmp(arg, "-img_trt") == 0)
-			{
-				if(i+1 > argc)
-					printf("No file specified\n%s\n", error);
-				else
-				{
-					char* file = argv[i+1];
-					i++;
-					display_image_test(file);
-				}
-			}
-			else
-			{
-				printf("Unknown argument \"%s\"\n%s\n", arg, error);
-			}
-		}
-		return 0;
-	}
 }
