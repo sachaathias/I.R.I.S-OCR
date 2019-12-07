55957f924a4109ba8c0f0f184052fbef8f025759
Etienne CHANUDET <etienne.chanudet@epita.fr>
2019-12-07 12:58:37 +0100

    envoi

--- neural_network/main_nn.c
+++ neural_network/main_nn.c
@@ -8,18 +8,19 @@ void train()
     double *matrix = malloc(sizeof(double)*28*28);
 
     load_weight_bias(net);
-    for(size_t epoch = 0; epoch < 10000000000; epoch++)
+    for(size_t epoch = 0; epoch < 1000000; epoch++)
     {
         char goal = get_random_matrix(matrix);
         char output = forward(net, matrix, goal);
 
-        if(epoch % 1 == 0)
+        if(epoch % 1000 == 0)
         {
             if(goal == output)
                 printf("%s%c --> %c | COST : %f%s\n", GREEN, goal, output, net->cost, DEFAULT);
             else
                 printf("%s%c --> %c | COST : %f%s\n", RED, goal, output, net->cost, DEFAULT);
             printf("%s", DEFAULT);
+            save_weight_bias(net);
         }
 
         backward(net);
